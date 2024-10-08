72549cc71dfa3e474388b89c3c28c48b8ab15843
r1407p <buszter0403@gmail.com>
2024-10-08 05:55:18 +0000

    feat: finish lab3

--- lab3/lab3.go
+++ lab3/lab3.go
@@ -9,8 +9,49 @@ import (
 )
 
 func Calculator(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/")
+	parts := strings.Split(path, "/")
+	if len(parts) != 3 {
+		fmt.Fprintf(w, "Error!")
+		return
+	}
+	var num1, num2 int
+	// num1, err1 := strconv.ParseFloat(parts[1], 64)
+	// num2, err2 := strconv.ParseFloat(parts[2], 64)
+	num1, err1 := strconv.Atoi(parts[1])
+	num2, err2 := strconv.Atoi(parts[2])
+
+	if err1 != nil || err2 != nil {
+		fmt.Fprintf(w, "Error!")
+		return
+	}
+
+	var result int
+	switch parts[0] {
+	case "add":
+		result = num1 + num2
+		fmt.Fprintf(w, "%d + %d = %d", num1, num2, result)
+	case "sub":
+		result = num1 - num2
+		fmt.Fprintf(w, "%d - %d = %d", num1, num2, result)
+	case "mul":
+		result = num1 * num2
+		fmt.Fprintf(w, "%d * %d = %d", num1, num2, result)
+	case "div":
+		if num2 == 0 {
+			fmt.Fprintf(w, "Error!")
+			return
+		}
+		result = num1 / num2
+		remainder := num1 % num2
+		fmt.Fprintf(w, "%d / %d = %d, reminder = %d", num1, num2, result, remainder)
+	default:
+		fmt.Fprintf(w, "Error!")
+		return
+	}
+
 	
-	
+
 
 }
 
